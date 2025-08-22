36f234c755b76c7b792c5b130d9dbe92469aeb9e
Vikas G <vikasgnsh76@gmail.com>
2025-08-22 22:27:20 +0530

    Update main.jsx

--- agri_frontend-main/src/main.jsx
+++ agri_frontend-main/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-
+import.meta.env.VITE_GOOGLE_API_KEY
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
